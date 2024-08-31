0d09d93f080a745db27cb490c9a2314815c1a319
Shubham Mishra <shubham.mishrakiit@gmail.com>
2024-08-31 09:53:27 +0530

    Implement responsive Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import skm_image from "../../assets/skm_image.jpg"
+import "./Hero.css"
 const Hero = () => {
     return (
-        <div>
+        <div className='hero'>
+            <img src={skm_image} alt="SKM Image" />
+            <h1><spna className='hero-name'>I'm Shubham Mishra ,</spna> frontend developer based in India.</h1>
+            <p>I am a frontend developer from Mumbai, India with 4 Years of experience in multiple companies like Cognizant, Tata Consltancy Services and HigRadius</p>
+            <div className='hero-action'>
+                <div className='hero-connect'>Connect With Me</div>
+                <div className='hero-resume'>Resume</div>
 
+            </div>
         </div>
     )
 }
